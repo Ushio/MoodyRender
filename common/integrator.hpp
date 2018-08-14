2ed51c2595f0181b2efdb2e3877bd4dff26da1a1
ushiostarfish <ushiostarfish@gmail.com>
2018-08-14 22:19:46 +0900

    コア部分をファイルに分離

---
+++ common/integrator.hpp
@@ -0,0 +1,298 @@
+﻿#pragma once
+
+#include <atomic>
+#include "scene_interface.hpp"
+
+#define DEBUG_MODE 0
+
+#define ENABLE_NEE 1
+#define ENABLE_NEE_MIS 1
+
+namespace rt {
+	class Image {
+	public:
+		Image(int w, int h) :_w(w), _h(h), _pixels(h * w), _randoms(h * w) {
+			XoroshiroPlus128 random;
+			for (int i = 0; i < _randoms.size(); ++i) {
+				_randoms[i] = random;
+				random.jump();
+			}
+		}
+		int width() const {
+			return _w;
+		}
+		int height() const {
+			return _h;
+		}
+
+		void add(int x, int y, glm::dvec3 c) {
+			int index = y * _w + x;
+			_pixels[index].color += c;
+			_pixels[index].sample++;
+		}
+
+		struct Pixel {
+			int sample = 0;
+			glm::dvec3 color;
+		};
+		const Pixel *pixel(int x, int y) const {
+			return _pixels.data() + y * _w + x;
+		}
+		Pixel *pixel(int x, int y) {
+			return _pixels.data() + y * _w + x;
+		}
+
+		PeseudoRandom *random(int x, int y) {
+			return _randoms.data() + y * _w + x;
+		}
+	private:
+		int _w = 0;
+		int _h = 0;
+		std::vector<Pixel> _pixels;
+		std::vector<XoroshiroPlus128> _randoms;
+	};
+
+	inline double GTerm(const glm::dvec3 &p, double cosThetaP, const glm::dvec3 &q, double cosThetaQ) {
+		return cosThetaP * cosThetaQ / glm::distance2(p, q);
+	}
+
+	inline bool has_value(const glm::dvec3 &c, double eps) {
+		return glm::any(glm::greaterThanEqual(c, glm::dvec3(eps)));
+	}
+
+	inline glm::dvec3 radiance(const rt::SceneInterface &scene, glm::dvec3 ro, glm::dvec3 rd, PeseudoRandom *random) {
+		const double kSceneEPS = scene.adaptiveEps();
+		// const double kSceneEPS = 1.0e-6;
+		const double kValueEPS = 1.0e-6;
+
+		glm::dvec3 Lo;
+		glm::dvec3 T(1.0);
+		Material previous_m;
+		double previous_pdf = 0.0;
+
+		bool inside = false;
+
+		constexpr int kDepth = 30;
+		for (int i = 0; i < kDepth; ++i) {
+			Material m;
+			float tmin = 0.0f;
+			glm::dvec3 wo = -rd;
+
+			if (scene.intersect(ro, rd, &m, &tmin)) {
+#if ENABLE_NEE
+				// NEE
+				// 最後のNEEは、PTと経路長をあわせるために、やらない
+				if (i != (kDepth - 1)) {
+					glm::dvec3 p = m->p;
+					for (auto it = scene.sampler_begin(); it != scene.sampler_end(); ++it) {
+						glm::dvec3 q;
+						glm::dvec3 n;
+						glm::dvec3 Le;
+						double pdf_area = 0.0;
+
+						if ((*it)->can_sample(p) == false) {
+							continue;
+						}
+
+						if (m->can_direct_sampling() == false) {
+							continue;
+						}
+
+						(*it)->sample(random, p, &q, &n, &Le, &pdf_area);
+
+						// 簡単なテスト
+						// if (std::abs(pdf_area - (*it)->pdf_area(p, q)) > 1.0e-6) { abort(); }
+
+						glm::dvec3 wi = glm::normalize(q - p);
+						glm::dvec3 bxdf = m->bxdf(wo, wi);
+						double cosThetaP = glm::dot(m->Ng, wi);
+						double cosThetaQ = glm::dot(n, -wi);
+
+						double g = GTerm(p, cosThetaP, q, cosThetaQ);
+
+						glm::dvec3 contribution = T * bxdf * Le * g / pdf_area;
+
+						if (has_value(contribution, kValueEPS)) {
+							if (scene.occluded(p + m->Ng * kSceneEPS, q + n * kSceneEPS) == false) {
+#if ENABLE_NEE_MIS
+								double this_pdf = pdf_area;
+								double other_pdf = m->pdf(wo, wi) * glm::dot(-n, wi) / glm::distance2(p, q);
+								// double mis_weight = this_pdf / (this_pdf + other_pdf);
+								double mis_weight = this_pdf * this_pdf / (this_pdf * this_pdf + other_pdf * other_pdf);
+								Lo += contribution * mis_weight;
+#else
+								Lo += contribution;
+#endif
+							}
+						}
+					}
+				}
+#endif
+				glm::dvec3 wi = m->sample(random, wo);
+				glm::dvec3 bxdf = m->bxdf(wo, wi);
+				glm::dvec3 emission = m->emission(wo);
+				double pdf = m->pdf(wo, wi);
+				double NoI = glm::dot(m->Ng, wi);
+				double cosTheta = std::abs(NoI);
+
+				if (inside) {
+					T *= m->beers_law(tmin);
+				}
+				bool over_boundary = NoI < 0.0;
+				if (over_boundary) {
+					inside = !inside;
+				}
+
+				glm::dvec3 contribution = emission * T;
+
+#if ENABLE_NEE_MIS
+				if (has_value(contribution, kValueEPS)) {
+					bool mis = false;
+					if (i != 0) {
+						if (auto sampler = m->direct_sampler()) {
+							if (sampler->can_sample(previous_m->p) && previous_m->can_direct_sampling()) {
+								double r = (double)tmin;
+								double this_pdf = previous_pdf * glm::dot(m->Ng, wo) / (r * r);
+								double other_pdf = sampler->pdf_area(previous_m->p, m->p);
+								// double mis_weight = this_pdf * this_pdf / (this_pdf + other_pdf);
+								double mis_weight = this_pdf * this_pdf / (this_pdf * this_pdf + other_pdf * other_pdf);
+								mis = true;
+								Lo += contribution * mis_weight;
+							}
+						}
+					}
+
+					if (mis == false) {
+						Lo += contribution;
+					}
+				}
+#elif ENABLE_NEE
+				if (i == 0) {
+					Lo += contribution;
+				}
+#else
+				Lo += contribution;
+#endif
+				if (has_value(bxdf, kValueEPS)) {
+					T *= bxdf * cosTheta / pdf;
+				}
+				else {
+					break;
+				}
+				if (has_value(T, 1.0e-6) == false) {
+					break;
+				}
+
+				// バイアスする方向は潜り込むときは逆転する
+				ro = m->p + (0.0 < NoI ? m->Ng : -m->Ng) * kSceneEPS;
+				rd = wi;
+
+				previous_pdf = pdf;
+			}
+			else {
+				break;
+			}
+			previous_m = m;
+		}
+		return Lo;
+	}
+
+	class PTRenderer {
+	public:
+		PTRenderer(std::shared_ptr<rt::Scene> scene)
+			: _scene(scene)
+			, _sceneInterface(new rt::SceneInterface(scene))
+			, _image(scene->camera.imageWidth(), scene->camera.imageHeight()) {
+			_badSampleNanCount = 0;
+			_badSampleInfCount = 0;
+			_badSampleNegativeCount = 0;
+			_badSampleFireflyCount = 0;
+		}
+		void step() {
+			_steps++;
+
+#if DEBUG_MODE
+			int focusX = 200;
+			int focusY = 200;
+
+			for (int y = 0; y < _scene->camera.imageHeight(); ++y) {
+				for (int x = 0; x < _scene->camera.imageWidth(); ++x) {
+					if (x != focusX || y != focusY) {
+						continue;
+					}
+					PeseudoRandom *random = _image.random(x, y);
+
+					glm::dvec3 o;
+					glm::dvec3 d;
+					_scene->camera.sampleRay(random, x, y, &o, &d);
+
+					auto r = radiance(*_sceneInterface, o, d, random);
+					_image.add(x, y, r);
+				}
+			}
+#else
+			tbb::parallel_for(tbb::blocked_range<int>(0, _scene->camera.imageHeight()), [&](const tbb::blocked_range<int> &range) {
+				for (int y = range.begin(); y < range.end(); ++y) {
+					for (int x = 0; x < _scene->camera.imageWidth(); ++x) {
+						PeseudoRandom *random = _image.random(x, y);
+						glm::dvec3 o;
+						glm::dvec3 d;
+						_scene->camera.sampleRay(random, x, y, &o, &d);
+
+						auto r = radiance(*_sceneInterface, o, d, random);
+
+						for (int i = 0; i < r.length(); ++i) {
+							if (glm::isnan(r[i])) {
+								_badSampleNanCount++;
+								r[i] = 0.0;
+							}
+							else if (glm::isfinite(r[i]) == false) {
+								_badSampleInfCount++;
+								r[i] = 0.0;
+							}
+							else if (r[i] < 0.0) {
+								_badSampleNegativeCount++;
+								r[i] = 0.0;
+							}
+							if (10000.0 < r[i]) {
+								_badSampleFireflyCount++;
+								r[i] = 0.0;
+							}
+						}
+						_image.add(x, y, r);
+					}
+				}
+			});
+#endif
+		}
+		int stepCount() const {
+			return _steps;
+		}
+
+		const rt::SceneInterface &sceneInterface() const {
+			return *_sceneInterface;
+		}
+
+		int badSampleNanCount() const {
+			return _badSampleNanCount.load();
+		}
+		int badSampleInfCount() const {
+			return _badSampleInfCount.load();
+		}
+		int badSampleNegativeCount() const {
+			return _badSampleNegativeCount.load();
+		}
+		int badSampleFireflyCount() const {
+			return _badSampleFireflyCount.load();
+		}
+
+		std::shared_ptr<rt::Scene> _scene;
+		std::shared_ptr<rt::SceneInterface> _sceneInterface;
+		Image _image;
+		int _steps = 0;
+		std::atomic<int> _badSampleNanCount;
+		std::atomic<int> _badSampleInfCount;
+		std::atomic<int> _badSampleNegativeCount;
+		std::atomic<int> _badSampleFireflyCount;
+	};
+}
---
+++ common/scene_interface.hpp
@@ -0,0 +1,217 @@
+﻿#pragma once
+#include <embree3/rtcore.h>
+
+#include "render_object.hpp"
+#include "microfacet.hpp"
+#include "bicubic.hpp"
+#include "material.hpp"
+#include "value_prportional_sampler.hpp"
+#include "geometry.hpp"
+#include "stopwatch.hpp"
+#include "direct_sampler.hpp"
+
+namespace rt {
+	inline void EmbreeErorrHandler(void* userPtr, RTCError code, const char* str) {
+		printf("Embree Error [%d] %s\n", code, str);
+	}
+	class SceneInterface {
+	public:
+		SceneInterface(std::shared_ptr<rt::Scene> scene) :_scene(scene) {
+			_embreeDevice = rtcNewDevice(nullptr);
+			rtcSetDeviceErrorFunction(_embreeDevice, EmbreeErorrHandler, nullptr);
+			_embreeScene = rtcNewScene(_embreeDevice);
+			rtcSetSceneBuildQuality(_embreeScene, RTC_BUILD_QUALITY_HIGH);
+			// RTC_BUILD_QUALITY_LOW, RTC_BUILD_QUALITY_MEDIUM, RTC_BUILD_QUALITY_HIGH
+
+			for (int i = 0; i < _scene->geometries.size(); ++i) {
+				RTCGeometry embreeGeometry = rtcNewGeometry(_embreeDevice, RTC_GEOMETRY_TYPE_TRIANGLE);
+
+				// バッファーはGeometryに結びつき、所有される
+				float *pVertexBuffer = (float *)rtcSetNewGeometryBuffer(embreeGeometry, RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, sizeof(float) * 3, _scene->geometries[i].points.size());
+				for (int j = 0; j < _scene->geometries[i].points.size(); ++j) {
+					auto p = _scene->geometries[i].points[j].P;
+
+					for (int k = 0; k < 3; ++k) {
+						pVertexBuffer[j * 3 + k] = p[k];
+					}
+				}
+				uint32_t *pIndexBuffer = (uint32_t *)rtcSetNewGeometryBuffer(embreeGeometry, RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, sizeof(uint32_t) * 3, _scene->geometries[i].primitives.size());
+				for (int j = 0; j < _scene->geometries[i].primitives.size(); ++j) {
+					auto indices = _scene->geometries[i].primitives[j].indices;
+					for (int k = 0; k < 3; ++k) {
+						pIndexBuffer[j * 3 + k] = indices[k];
+					}
+				}
+				rtcCommitGeometry(embreeGeometry);
+				rtcAttachGeometryByID(_embreeScene, embreeGeometry, i);
+				rtcReleaseGeometry(embreeGeometry);
+			}
+
+			rtcCommitScene(_embreeScene);
+
+			rtcInitIntersectContext(&_context);
+
+
+			for (int i = 0; i < _scene->geometries.size(); ++i) {
+				Geometry& g = _scene->geometries[i];
+				SphericalRectangleSampler *previous_sr_sampler = nullptr;
+
+				for (int j = 0; j < g.primitives.size(); ++j) {
+					Geometry::Primitive &p = g.primitives[j];
+					IMaterial *m = g.primitives[j].material.get();
+					LambertianMaterial *lambertian = dynamic_cast<LambertianMaterial *>(m);
+					if (lambertian && lambertian->isEmission()) {
+						if (auto sample = lambertian->samplingStrategy.get<AreaSample>()) {
+							auto a = g.points[p.indices[0]].P;
+							auto b = g.points[p.indices[1]].P;
+							auto c = g.points[p.indices[2]].P;
+							TriangleAreaSampler *sampler = new TriangleAreaSampler(a, b, c, lambertian->backEmission, lambertian->Le);
+							lambertian->sampler = sampler;
+							_directSamplers.emplace_back(sampler);
+						}
+						else if (auto sample = lambertian->samplingStrategy.get<SphericalRectangleSample>())
+						{
+							// あまり綺麗ではないが、triangleIndex 0, 1, 0, 1...となる決まりにする。
+							if (sample->triangleIndex == 0) {
+								SphericalRectangleSampler *sampler = new SphericalRectangleSampler(sample->s, sample->ex, sample->ey, lambertian->backEmission, lambertian->Le);
+								previous_sr_sampler = sampler;
+								lambertian->sampler = sampler;
+								_directSamplers.emplace_back(sampler);
+							}
+							else {
+								lambertian->sampler = previous_sr_sampler;
+							}
+						}
+					}
+				}
+			}
+
+			RTCBounds bounds;
+			rtcGetSceneBounds(_embreeScene, &bounds);
+
+			float maxWide = bounds.upper_x - bounds.lower_x;
+			maxWide = std::max(maxWide, bounds.upper_y - bounds.lower_y);
+			maxWide = std::max(maxWide, bounds.upper_z - bounds.lower_z);
+
+			_sceneAdaptiveEps = std::max(maxWide * 1.0e-5, 1.0e-5);
+			// printf("_sceneAdaptiveEps %.10f\n", _sceneAdaptiveEps);
+		}
+		~SceneInterface() {
+			rtcReleaseScene(_embreeScene);
+			rtcReleaseDevice(_embreeDevice);
+		}
+		SceneInterface(const SceneInterface &) = delete;
+		void operator=(const SceneInterface &) = delete;
+
+		double adaptiveEps() const {
+			return _sceneAdaptiveEps;
+		}
+
+		bool occluded(const glm::dvec3 &p, const glm::dvec3 &q) const {
+			glm::dvec3 rd = q - p;
+
+			RTCRay ray;
+			ray.org_x = p.x;
+			ray.org_y = p.y;
+			ray.org_z = p.z;
+			ray.dir_x = rd.x;
+			ray.dir_y = rd.y;
+			ray.dir_z = rd.z;
+			ray.time = 0.0;
+
+			ray.tfar = 1.0f;
+			ray.tnear = 0.0f;
+
+			ray.mask = 0;
+			ray.id = 0;
+			ray.flags = 0;
+
+			rtcOccluded1(_embreeScene, &_context, &ray);
+
+			return ray.tfar != 1.0f;
+		}
+
+		bool intersect(const glm::dvec3 &ro, const glm::dvec3 &rd, Material *material, float *tmin) const {
+			RTCRayHit rayhit;
+			rayhit.ray.org_x = ro.x;
+			rayhit.ray.org_y = ro.y;
+			rayhit.ray.org_z = ro.z;
+			rayhit.ray.dir_x = rd.x;
+			rayhit.ray.dir_y = rd.y;
+			rayhit.ray.dir_z = rd.z;
+			rayhit.ray.time = 0.0f;
+
+			rayhit.ray.tfar = FLT_MAX;
+			rayhit.ray.tnear = 0.0f;
+
+			rayhit.ray.mask = 0;
+			rayhit.ray.id = 0;
+			rayhit.ray.flags = 0;
+			rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+			rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
+			rtcIntersect1(_embreeScene, &_context, &rayhit);
+
+			if (rayhit.hit.geomID == RTC_INVALID_GEOMETRY_ID) {
+				return false;
+			}
+
+			*tmin = rayhit.ray.tfar;
+
+			int index = rayhit.hit.geomID;
+			const auto &geom = _scene->geometries[index];
+			const auto &prim = geom.primitives[rayhit.hit.primID];
+			*material = prim.material;
+
+			glm::dvec3 Ng = prim.Ng;
+
+			// 裏面
+			bool backfacing = false;
+			if (glm::dot(rd, Ng) > 0.0)
+			{
+				Ng = -Ng;
+				backfacing = true;
+			}
+
+			(*material)->Ng = Ng;
+			(*material)->backfacing = backfacing;
+
+			/*
+			https://embree.github.io/api.html
+			t_uv = (1-u-v)*t0 + u*t1 + v*t2
+			= t0 + u*(t1-t0) + v*(t2-t0)
+			*/
+			//double u = rayhit.hit.u;
+			//double v = rayhit.hit.v;
+			//auto v0 = geom.points[prim.indices[0]].P;
+			//auto v1 = geom.points[prim.indices[1]].P;
+			//auto v2 = geom.points[prim.indices[2]].P;
+			//(*material)->p = (1.0 - u - v) * v0 + u * v1 + v * v2;
+
+			(*material)->p = ro + rd * double(*tmin);
+			return true;
+		}
+
+		const Camera &camera() const {
+			return _scene->camera;
+		}
+
+		std::vector<std::unique_ptr<IDirectSampler>>::const_iterator sampler_begin() const {
+			return _directSamplers.begin();
+		}
+		std::vector<std::unique_ptr<IDirectSampler>>::const_iterator sampler_end() const {
+			return _directSamplers.end();
+		}
+		int samplerCount() const {
+			return _directSamplers.size();
+		}
+
+		std::shared_ptr<rt::Scene> _scene;
+		RTCDevice _embreeDevice = nullptr;
+		RTCScene _embreeScene = nullptr;
+		mutable RTCIntersectContext _context;
+
+		std::vector<std::unique_ptr<IDirectSampler>> _directSamplers;
+
+		double _sceneAdaptiveEps = 0.0f;
+	};
+}
